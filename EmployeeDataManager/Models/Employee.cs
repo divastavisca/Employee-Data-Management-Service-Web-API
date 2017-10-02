e1a29f4530b48aa154917951191ecb87cc814c4e
divastavisca <dagarwal@tavisca.com>
2017-10-02 16:38:22 +0530

    added api solution

---
+++ EmployeeDataManager/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDataManager.Models
+{
+    public class Employee
+    {
+        public string Id { get; private set; }
+        public Name Name { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string EmailId { get; private set; }
+        public string DepartmentId { get; private set; }
+
+        public Employee(string id,Name name,DateTime dateOfBirth,string emailId,string departmentId)
+        {
+            Id = id;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            EmailId = emailId;
+            DepartmentId = departmentId;
+        }
+    }
+}
---
+++ EmployeeDataManager/Models/Name.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace EmployeeDataManager.Models
+{
+    public class Name
+    {
+        public string FirstName { get; private set; }
+        public string MiddleName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Name(string firstName,string middleName,string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(FirstName+" ");
+            if (MiddleName != null)
+                builder.Append(MiddleName+" ");
+            builder.Append(LastName);
+            return builder.ToString();
+        }
+    }
+}
