44c2c1f663cdff5052e3c9109d3a228f957f1266
divastavisca <dagarwal@tavisca.com>
2017-10-02 17:00:35 +0530

    added abstract data source contract

---
+++ EmployeeDataManager/Contracts/IEmployeeDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeDataManager.Models;
+
+namespace EmployeeDataManager.Contracts
+{
+    interface IEmployeeDataManager
+    {
+        bool Add(string firstName, string middleName, string lastName, string dateOfBirth, string emailId, string departmentId);
+        List<Employee> GetAll();
+        Employee Get(string id);
+        bool Update(string id, Name name, string emailId, string departmentId);
+        bool DeleteAll();
+        bool Delete(string id);
+    }
+}
