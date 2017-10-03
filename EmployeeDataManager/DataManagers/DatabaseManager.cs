8e863c5d29535a7bdc149b6481e668b7b7366a5f
Divas Agarwal <dagarwal@tavisca.com>
2017-10-03 14:41:12 +0530

    minor change

--- EmployeeDataManager/DataManagers/DatabaseManager.cs
+++ EmployeeDataManager/DataManagers/DatabaseManager.cs
@@ -50,6 +50,7 @@ namespace EmployeeDataManager.DataManagers
                 return false;
             }
         }
+
         public bool GetAll(out List<Employee> employees)
         {
             DataTable queryResult = new DataTable();
@@ -72,14 +73,14 @@ namespace EmployeeDataManager.DataManagers
             }
             if(queryResult.Rows.Count>0)
             {
-                foreach(DataRow row in queryResult.Rows)
+                foreach(DataRow row  in queryResult.Rows)
                 {
                     EmployeeData empData = new EmployeeData
                         (
                             new Name
                             (
                                 row["FirstName"].ToString(),
-                                row["MiddleName"] != null ? row["MiddleName"].ToString() : null,
+                                row["MiddleName"].ToString() != "" ? row["MiddleName"].ToString() : null,
                                 row["LastName"].ToString()
                             ),
                             DateTime.Parse(row["DateOfBirth"].ToString()),
@@ -96,6 +97,7 @@ namespace EmployeeDataManager.DataManagers
                 return false;
             }
         }
+
         public bool Get(string id, out Employee employee)
         {
             if(_cache.ContainsKey(id))
@@ -149,14 +151,59 @@ namespace EmployeeDataManager.DataManagers
                 }
             }
         }
+
         public bool Update(string id, EmployeeData employeeData)
         {
-            throw new NotImplementedException();
+            DataTable queryResult = new DataTable();
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = "Insert into Employee(FirstName,MiddleName,LastName,DateOfBirth,EmailId,DepartmentId) VALUES(@FirstName,@MiddleName,@LastName,@DateOfBirth,@EmailId,@DepartmentId)";
+                sqlCommand.Parameters.AddWithValue("FirstName", employeeData.Name.FirstName);
+                sqlCommand.Parameters.AddWithValue("MiddleName", employeeData.Name.MiddleName);
+                sqlCommand.Parameters.AddWithValue("LastName", employeeData.Name.LastName);
+                sqlCommand.Parameters.AddWithValue("DateOfBirth", employeeData.DateOfBirth);
+                sqlCommand.Parameters.AddWithValue("EmailId", employeeData.EmailId);
+                sqlCommand.Parameters.AddWithValue("DepartmentId", employeeData.DepartmentId);
+                sqlCommand.Connection = connection;
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCommand);
+                try
+                {
+                    dataAdapter.Fill(queryResult);
+                }
+                catch
+                {
+                    return false;
+                }
+                if(_cache.ContainsKey(id))
+                {
+                    _cache[id] = getEmployee(id, employeeData);
+                }
+                return true;
+            }
         }
+
         public bool DeleteAll()
         {
-            throw new NotImplementedException();
+            DataTable queryResult = new DataTable();
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = "Delete from Employee";
+                sqlCommand.Connection = connection;
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCommand);
+                try
+                {
+                    dataAdapter.Fill(queryResult);
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
+            }
         }
+
         public bool Delete(string id)
         {
             throw new NotImplementedException();
